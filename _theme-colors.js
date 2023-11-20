26ea178b19970eb88b5e95483060100f5f9e7573
nanodotbot <nanodotbot@gmail.com>
2023-11-20 16:14:59 +0100

    re-fractured boilerplate - neat design

---
+++ _theme-colors.js
@@ -0,0 +1,77 @@
+const lightBlue = {
+    '--theme-color-10': 'var(--blue-10)',
+    '--theme-color-9': 'var(--blue-9)',
+    '--theme-color-8': 'var(--blue-8)',
+    '--theme-color-7': 'var(--blue-7)',
+    '--theme-color-6': 'var(--blue-6)',
+    '--theme-color-5': 'var(--blue-5)',
+    '--theme-color-4': 'var(--blue-4)',
+    '--theme-color-3': 'var(--blue-3)',
+    '--theme-color-2': 'var(--blue-2)',
+    '--theme-color-1': 'var(--blue-1)'
+};
+
+const darkBlue = {
+    '--theme-color-10': 'var(--blue-10)',
+    '--theme-color-9': 'var(--blue-9)',
+    '--theme-color-8': 'var(--blue-8)',
+    '--theme-color-7': 'var(--blue-7)',
+    '--theme-color-6': 'var(--blue-6)',
+    '--theme-color-5': 'var(--blue-5)',
+    '--theme-color-4': 'var(--blue-4)',
+    '--theme-color-3': 'var(--blue-3)',
+    '--theme-color-2': 'var(--blue-2)',
+    '--theme-color-1': 'var(--blue-1)'
+};
+
+const lightGreen = {
+    '--theme-color-10': 'var(--green-10)',
+    '--theme-color-9': 'var(--green-9)',
+    '--theme-color-8': 'var(--green-8)',
+    '--theme-color-7': 'var(--green-7)',
+    '--theme-color-6': 'var(--green-6)',
+    '--theme-color-5': 'var(--green-5)',
+    '--theme-color-4': 'var(--green-4)',
+    '--theme-color-3': 'var(--green-3)',
+    '--theme-color-2': 'var(--green-2)',
+    '--theme-color-1': 'var(--green-1)'
+};
+
+const darkGreen = {
+    '--theme-color-10': 'var(--green-10)',
+    '--theme-color-9': 'var(--green-9)',
+    '--theme-color-8': 'var(--green-8)',
+    '--theme-color-7': 'var(--green-7)',
+    '--theme-color-6': 'var(--green-6)',
+    '--theme-color-5': 'var(--green-5)',
+    '--theme-color-4': 'var(--green-4)',
+    '--theme-color-3': 'var(--green-3)',
+    '--theme-color-2': 'var(--green-2)',
+    '--theme-color-1': 'var(--green-1)'
+};
+
+const lightRed = {
+    '--theme-color-10': 'var(--red-10)',
+    '--theme-color-9': 'var(--red-9)',
+    '--theme-color-8': 'var(--red-8)',
+    '--theme-color-7': 'var(--red-7)',
+    '--theme-color-6': 'var(--red-6)',
+    '--theme-color-5': 'var(--red-5)',
+    '--theme-color-4': 'var(--red-4)',
+    '--theme-color-3': 'var(--red-3)',
+    '--theme-color-2': 'var(--red-2)',
+    '--theme-color-1': 'var(--red-1)'
+};
+
+const darkRed = {
+    '--theme-color-10': 'var(--red-10)',
+    '--theme-color-9': 'var(--red-9)',
+    '--theme-color-8': 'var(--red-8)',
+    '--theme-color-7': 'var(--red-7)',
+    '--theme-color-6': 'var(--red-6)',
+    '--theme-color-5': 'var(--red-5)',
+    '--theme-color-4': 'var(--red-4)',
+    '--theme-color-3': 'var(--red-3)',
+    '--theme-color-2': 'var(--red-2)',
+    '--theme-color-1': 'var(--red-1)'
+};
---
+++ _theme-themes.js
@@ -0,0 +1,134 @@
+const lightThemeBlue = {
+    '--theme': 'var(--white)',
+    '--theme-contrast': 'var(--theme-shades-7)',
+    '--background-transparent-theme': 'var(--background-transparent-light)',
+
+    '--theme-color-10': 'var(--blue-10)',
+    '--theme-color-9': 'var(--blue-9)',
+    '--theme-color-8': 'var(--blue-8)',
+    '--theme-color-7': 'var(--blue-7)',
+    '--theme-color-6': 'var(--blue-6)',
+    '--theme-color-5': 'var(--blue-5)',
+    '--theme-color-4': 'var(--blue-4)',
+    '--theme-color-3': 'var(--blue-3)',
+    '--theme-color-2': 'var(--blue-2)',
+    '--theme-color-1': 'var(--blue-1)',
+
+    '--link-text': 'var(--theme-color-6)',
+    '--link-hover-text': 'var(--text)',
+    '--link-hover-bg': 'var(--theme-color-1)',
+    '--selection-text': 'var(--text)',
+    '--selection-bg': 'var(--theme-color-1)'
+};
+
+const darkThemeBlue = {
+    '--theme': 'var(--theme-shades-9)',
+    '--theme-contrast': 'var(--theme-shades-1)',
+    '--background-transparent-theme': 'var(--background-transparent-dark)',
+
+    '--theme-color-10': 'var(--blue-10)',
+    '--theme-color-9': 'var(--blue-9)',
+    '--theme-color-8': 'var(--blue-8)',
+    '--theme-color-7': 'var(--blue-7)',
+    '--theme-color-6': 'var(--blue-6)',
+    '--theme-color-5': 'var(--blue-5)',
+    '--theme-color-4': 'var(--blue-4)',
+    '--theme-color-3': 'var(--blue-3)',
+    '--theme-color-2': 'var(--blue-2)',
+    '--theme-color-1': 'var(--blue-1)',
+
+    '--link-text': 'var(--theme-color-5)',
+    '--link-hover-text': 'var(--theme-color-4)',
+    '--link-hover-bg': 'var(--theme)',
+    '--selection-text': 'var(--theme-color-2)',
+    '--selection-bg': 'transparent'
+};
+const lightThemeGreen = {
+    '--theme': 'var(--white)',
+    '--theme-contrast': 'var(--theme-shades-7)',
+
+    '--theme-color-10': 'var(--green-10)',
+    '--theme-color-9': 'var(--green-9)',
+    '--theme-color-8': 'var(--green-8)',
+    '--theme-color-7': 'var(--green-7)',
+    '--theme-color-6': 'var(--green-6)',
+    '--theme-color-5': 'var(--green-5)',
+    '--theme-color-4': 'var(--green-4)',
+    '--theme-color-3': 'var(--green-3)',
+    '--theme-color-2': 'var(--green-2)',
+    '--theme-color-1': 'var(--green-1)',
+
+    '--link-text': 'var(--theme-color-6)',
+    '--link-hover-text': 'var(--text)',
+    '--link-hover-bg': 'var(--theme-color-1)',
+    '--selection-text': 'var(--text)',
+    '--selection-bg': 'var(--theme-color-1)'
+};
+
+const darkThemeGreen = {
+    '--theme': 'var(--theme-shades-9)',
+    '--theme-contrast': 'var(--theme-shades-1)',
+    '--background-transparent-theme': 'var(--background-transparent-dark)',
+
+    '--theme-color-10': 'var(--green-10)',
+    '--theme-color-9': 'var(--green-9)',
+    '--theme-color-8': 'var(--green-8)',
+    '--theme-color-7': 'var(--green-7)',
+    '--theme-color-6': 'var(--green-6)',
+    '--theme-color-5': 'var(--green-5)',
+    '--theme-color-4': 'var(--green-4)',
+    '--theme-color-3': 'var(--green-3)',
+    '--theme-color-2': 'var(--green-2)',
+    '--theme-color-1': 'var(--green-1)',
+
+    '--link-text': 'var(--theme-color-5)',
+    '--link-hover-text': 'var(--theme-color-4)',
+    '--link-hover-bg': 'var(--theme)',
+    '--selection-text': 'var(--theme-color-2)',
+    '--selection-bg': 'transparent'
+};
+const lightThemeRed = {
+    '--theme': 'var(--white)',
+    '--theme-contrast': 'var(--theme-shades-7)',
+    '--background-transparent-theme': 'var(--background-transparent-light)',
+
+    '--theme-color-10': 'var(--red-10)',
+    '--theme-color-9': 'var(--red-9)',
+    '--theme-color-8': 'var(--red-8)',
+    '--theme-color-7': 'var(--red-7)',
+    '--theme-color-6': 'var(--red-6)',
+    '--theme-color-5': 'var(--red-5)',
+    '--theme-color-4': 'var(--red-4)',
+    '--theme-color-3': 'var(--red-3)',
+    '--theme-color-2': 'var(--red-2)',
+    '--theme-color-1': 'var(--red-1)',
+
+    '--link-text': 'var(--theme-color-6)',
+    '--link-hover-text': 'var(--text)',
+    '--link-hover-bg': 'var(--theme-color-1)',
+    '--selection-text': 'var(--text)',
+    '--selection-bg': 'var(--theme-color-1)'
+};
+
+const darkThemeRed = {
+    '--theme': 'var(--theme-shades-9)',
+    '--theme-contrast': 'var(--theme-shades-1)',
+    '--background-transparent-theme': 'var(--background-transparent-dark)',
+
+    '--theme-color-10': 'var(--red-10)',
+    '--theme-color-9': 'var(--red-9)',
+    '--theme-color-8': 'var(--red-8)',
+    '--theme-color-7': 'var(--red-7)',
+    '--theme-color-6': 'var(--red-6)',
+    '--theme-color-5': 'var(--red-5)',
+    '--theme-color-4': 'var(--red-4)',
+    '--theme-color-3': 'var(--red-3)',
+    '--theme-color-2': 'var(--red-2)',
+    '--theme-color-1': 'var(--red-1)',
+
+    '--link-text': 'var(--theme-color-5)',
+    '--link-hover-text': 'var(--theme-color-4)',
+    '--link-hover-bg': 'var(--theme)',
+    '--selection-text': 'var(--theme-color-2)',
+    '--selection-bg': 'transparent'
+};
